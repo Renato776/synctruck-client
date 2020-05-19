8767a06e254ee9b1e4d84926f3bde1e3c8f36208
Renato Flores <renatojosuefloresperez@gmail.com>
2020-05-19 10:59:26 -0600

    Initial commit

--- server/Printing.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Printing {
-    static log(text) {
-        console.log(text);
-    }
-}
-exports.Printing = Printing;
